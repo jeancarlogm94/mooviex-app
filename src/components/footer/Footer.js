ed1fd12c7a6ec935138b12de86ed5fc45ea5e7b7
John Doe <johndoe@example.com>
2022-08-23 21:35:05 -0500

    first commit

--- src/components/footer/Footer.js
+++ src/components/footer/Footer.js
@@ -14,7 +14,7 @@ const Footer = () => {
         <div className="footer__content__logo">
           <div className="logo">
             <img src={logo} alt="" />
-            <Link to="/">Moviex</Link>
+            <Link to="/">Mooviex</Link>
           </div>
         </div>
         <div className="footer__content__menus">
--- src/components/header/Header.js
+++ src/components/header/Header.js
@@ -49,7 +49,7 @@ const Header = () => {
       <div className="header__wrap container">
         <div className="logo">
           <img src={logo} alt="" />
-          <Link to="/">Moviex App</Link>
+          <Link to="/">Mooviex App</Link>
         </div>
         <ul className="header__nav">
           {headerNav.map((e, i) => (
